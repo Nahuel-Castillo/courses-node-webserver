8adbe7c3d6510d21e5a5119c432a9e1cb97fcef5
Nahuel_Castillo <nahuelcastillo13@gmail.com>
2021-03-01 17:20:15 -0600

    Web server ready

---
+++ app-old.js
@@ -0,0 +1,13 @@
+
+// const http = require('http');
+
+// http.createServer( ( req, res ) =>  {
+
+//     // console.log( req );
+//     res.write('hello world');
+//     res.end();
+
+// })
+// .listen( 4003 );
+
+// console.log('Listen port', 4003 );
---
+++ app.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+//Handlebars
+app.set( 'view engine', 'hbs' );
+hbs.registerPartials( __dirname + '/views/partials');
+
+const port  = process.env.PORT;
+
+//static content
+app.use( express.static('public') );
+ 
+//routes using hbs
+app.get('/', ( req, res ) => {
+    res.render('home', { //with hbs
+        title: 'Node curse', //send to view
+        author: 'Nahuel'
+    }); 
+});
+
+app.get('/generic', ( req, res ) => {
+    
+    res.render( 'generic', { //with hbs
+        title: 'Node curse', //send to view
+        author: 'Nahuel'
+    });
+});
+
+app.get('/elements', ( req, res ) => {
+    res.render('elements', { //with hbs
+        title: 'Node curse', //send to view
+        author: 'Nahuel'
+    });
+});
+
+
+//normal routes
+// app.get('/', (req, res) => {
+//   res.send('Root route');
+// });
+// app.get('/generic', ( req, res ) => {
+//     res.sendFile( __dirname + '/public/generic.html' );
+// });
+// app.get('/elements', ( req, res ) => {
+//     res.sendFile( __dirname + '/public/elements.html' );
+// });
+
+app.get('*', ( req, res ) => {
+    res.status( 404 ).send('404 | Page not found! ');
+});
+ 
+app.listen( port, () => {
+    console.log(`Server running on ${ port } port`);
+} );
